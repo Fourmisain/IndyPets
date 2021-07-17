d1651b05d87718eea29034272ca9b0a3ef661116
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-07-17 09:56:07 +0200

    show pet name in text message

--- src/main/java/com/lizin5ths/indypets/IndyPets.java
+++ src/main/java/com/lizin5ths/indypets/IndyPets.java
@@ -13,18 +13,18 @@ public class IndyPets implements ModInitializer {
 	public static final String MOD_ID = "indypets";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-	private static final Text INDEPENDENT_TEXT = new TranslatableText("text.indypets.independent");
-	private static final Text FOLLOWING_TEXT = new TranslatableText("text.indypets.following");
-
 	public static boolean changeFollowing(PlayerEntity player, TameableEntity tameable) {
 		Follower follower = (Follower) tameable;
 
 		if (IndyPetsConfig.getSelectiveFollowing() && tameable.isOwner(player) && player.isSneaking()) {
+			follower.setFollowing(!follower.isFollowing());
+
 			if (!IndyPetsConfig.getSilentMode()) {
-				player.sendSystemMessage(follower.isFollowing() ? INDEPENDENT_TEXT : FOLLOWING_TEXT, Util.NIL_UUID);
+				String key = follower.isFollowing() ? "text.indypets.following" : "text.indypets.independent";
+				Text text = new TranslatableText(key, tameable.getName().getString());
+				player.sendSystemMessage(text, Util.NIL_UUID);
 			}
 
-			follower.setFollowing(!follower.isFollowing());
 			return true;
 		}
 
