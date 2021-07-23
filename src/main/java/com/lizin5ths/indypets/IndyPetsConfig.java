4a03225b2de76b0952738e3408004124b013dd87
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-07-23 05:06:09 +0200

    add dummy IndyPetsConfig, downgrade yarn mapping to make the project compile

---
+++ src/main/java/com/lizin5ths/indypets/IndyPetsConfig.java
@@ -0,0 +1,27 @@
+package com.lizin5ths.indypets;
+
+public class IndyPetsConfig {
+	public static void load() {
+
+	}
+
+	public static boolean getSelectiveFollowing() {
+		return true;
+	}
+
+	public static boolean getSilentMode() {
+		return false;
+	}
+
+	public static boolean getDisableCatFollow() {
+		return false;
+	}
+
+	public static boolean getDisableParrotFollow() {
+		return false;
+	}
+
+	public static boolean getDisableWolfFollow() {
+		return false;
+	}
+}
