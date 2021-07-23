141c216078c7e09c7583bca6d88a7f4a512844f6
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-07-23 06:54:33 +0200

    add translations to config screen

--- src/main/java/com/lizin5ths/indypets/IndyPetsConfig.java
+++ src/main/java/com/lizin5ths/indypets/IndyPetsConfig.java
@@ -1,15 +1,21 @@
 package com.lizin5ths.indypets;
 
-
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = IndyPets.MOD_ID)
 public class IndyPetsConfig implements ConfigData {
-	public boolean selectiveFollowing = true;
-	public boolean silentMode = false;
 
+	@ConfigEntry.Gui.Tooltip(count = 2)
+	public boolean selectiveFollowing = true;
+	@ConfigEntry.Gui.Tooltip
 	public boolean disableCatFollow = true;
+	@ConfigEntry.Gui.Tooltip
 	public boolean disableParrotFollow = true;
+	@ConfigEntry.Gui.Tooltip
 	public boolean disableWolfFollow = true;
+
+	@ConfigEntry.Gui.Tooltip
+	public boolean silentMode = false;
 }
