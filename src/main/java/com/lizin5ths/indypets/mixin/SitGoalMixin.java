45787d6bad28df0a6a85a48cc71688c759cb1468
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2022-07-27 05:12:43 +0200

    don't sit down independent pets when their owner disconnects

---
+++ src/main/java/com/lizin5ths/indypets/mixin/SitGoalMixin.java
@@ -0,0 +1,29 @@
+package com.lizin5ths.indypets.mixin;
+
+import com.lizin5ths.indypets.util.Follower;
+import net.minecraft.entity.ai.goal.SitGoal;
+import net.minecraft.entity.passive.TameableEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(SitGoal.class)
+public abstract class SitGoalMixin {
+    @Shadow @Final
+    private TameableEntity tameable;
+
+    @ModifyConstant(
+        method = "canStart",
+        constant = @Constant(intValue = 1)
+    )
+    public int dontSitOnLogoutWhenIndependent(int canStart) {
+        Follower follower = (Follower) tameable;
+
+        if (!follower.isFollowing())
+            return 0;
+
+        return canStart;
+    }
+}
