033ed32a3cf29045b2e6fb3b39a798f88513555f
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2022-08-21 04:08:58 +0200

    fix whistle toggle (counting the same button twice)

--- src/main/java/com/lizin5ths/indypets/client/Keybindings.java
+++ src/main/java/com/lizin5ths/indypets/client/Keybindings.java
@@ -1,5 +1,6 @@
 package com.lizin5ths.indypets.client;
 
+import com.lizin5ths.indypets.IndyPets;
 import com.lizin5ths.indypets.IndyPetsClient;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
@@ -47,7 +48,7 @@ public class Keybindings {
 
             // when both actions are bound to the same key, switch between the actions (whistle -> unwhistle -> whistle etc)
             if (!whistle.isUnbound() && whistle.equals(unwhistle)) {
-                if ((whistleCount + unwhistleCount) % 2 == 1) {
+                if (whistleCount % 2 == 1) {
                     whistleToggle = !whistleToggle;
                 }
                 shouldWhistle = whistleToggle;
