983403b09f2bcaaf3d5579c211468b9c67f93682
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-07-22 14:18:51 +0200

    dodge the duplicate key check for whistle keys

--- src/main/java/com/lizin5ths/indypets/client/Keybindings.java
+++ src/main/java/com/lizin5ths/indypets/client/Keybindings.java
@@ -1,6 +1,7 @@
 package com.lizin5ths.indypets.client;
 
 import com.lizin5ths.indypets.IndyPetsClient;
+import com.lizin5ths.indypets.mixin.access.KeyBindingAccessor;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
@@ -11,20 +12,34 @@ import static com.lizin5ths.indypets.IndyPetsClient.UNWHISTLE;
 import static com.lizin5ths.indypets.IndyPetsClient.WHISTLE;
 
 public class Keybindings {
+	public static class WhistleKeyBinding extends KeyBinding {
+		public WhistleKeyBinding(String translationKey) {
+			super(translationKey, GLFW.GLFW_KEY_J, "key.categories.indypets");
+		}
+
+		@Override
+		public boolean equals(KeyBinding other) {
+			// lie about whistle keys being equal (using the same key) to dodge the duplicate check
+			if (this == WHISTLE_KEY && other == UNWHISTLE_KEY
+				|| this == UNWHISTLE_KEY && other == WHISTLE_KEY)
+				return false;
+
+			return super.equals(other);
+		}
+	}
+
+	public static InputUtil.Key getBoundKey(KeyBinding keyBinding) {
+		return ((KeyBindingAccessor) keyBinding).getBoundKey();
+	}
+
+	public static final WhistleKeyBinding WHISTLE_KEY   = new WhistleKeyBinding("key.indypets.whistle");
+	public static final WhistleKeyBinding UNWHISTLE_KEY = new WhistleKeyBinding("key.indypets.unwhistle");
+
 	private static boolean whistleToggle = false;
 
 	public static void init() {
-		KeyBinding whistle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-			"key.indypets.whistle",
-			InputUtil.Type.KEYSYM,
-			GLFW.GLFW_KEY_J,
-			"key.categories.indypets"));
-
-		KeyBinding unwhistle = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-			"key.indypets.unwhistle",
-			InputUtil.Type.KEYSYM,
-			GLFW.GLFW_KEY_J,
-			"key.categories.indypets"));
+		KeyBindingHelper.registerKeyBinding(WHISTLE_KEY);
+		KeyBindingHelper.registerKeyBinding(UNWHISTLE_KEY);
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.player == null) return;
@@ -33,10 +48,10 @@ public class Keybindings {
 			int whistleCount = 0;
 			int unwhistleCount = 0;
 
-			while (whistle.wasPressed()) {
+			while (WHISTLE_KEY.wasPressed()) {
 				whistleCount++;
 			}
-			while (unwhistle.wasPressed()) {
+			while (UNWHISTLE_KEY.wasPressed()) {
 				unwhistleCount++;
 			}
 
@@ -46,7 +61,7 @@ public class Keybindings {
 			boolean shouldWhistle; // else unwhistle
 
 			// when both actions are bound to the same key, switch between the actions (whistle -> unwhistle -> whistle etc)
-			if (!whistle.isUnbound() && whistle.equals(unwhistle)) {
+			if (!WHISTLE_KEY.isUnbound() && getBoundKey(WHISTLE_KEY).equals(getBoundKey(UNWHISTLE_KEY))) {
 				// with Amecs, both key bindings count, in vanilla only one counts, so we check the max of the two
 				if (Math.max(whistleCount, unwhistleCount) % 2 == 1) {
 					whistleToggle = !whistleToggle;
