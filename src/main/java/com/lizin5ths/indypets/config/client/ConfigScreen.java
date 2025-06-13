86afef864bbc5270ba1447937d3cf26ed6a2d74c
Gili "OpenBagTwo" Barlev <openbagtwo@gmail.com>
2025-06-13 10:16:07 -0400

    Update build target to 1.21.6(ish)
    and temporarily disable modmenu integration

--- src/main/java/com/lizin5ths/indypets/config/client/ConfigScreen.java
+++ src/main/java/com/lizin5ths/indypets/config/client/ConfigScreen.java
@@ -1,13 +1,13 @@
 package com.lizin5ths.indypets.config.client;
 
 import com.lizin5ths.indypets.config.Config;
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
-
-public class ConfigScreen implements ModMenuApi {
-	@Override
-	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return parent -> AutoConfig.getConfigScreen(Config.class, parent).get();
-	}
-}
+//import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+//import com.terraformersmc.modmenu.api.ModMenuApi;
+//import me.shedaniel.autoconfig.AutoConfig;
+//
+//public class ConfigScreen implements ModMenuApi {
+//	@Override
+//	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+//		return parent -> AutoConfig.getConfigScreen(Config.class, parent).get();
+//	}
+//}
