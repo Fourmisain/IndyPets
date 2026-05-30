eb31f351114c0c0649cc760b64b0452f88070c26
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-05-30 23:07:36 +0200

    workaround broken cloth config item dropdown when no world is loaded

--- src/main/java/com/lizin5ths/indypets/config/client/ItemGuiProvider.java
+++ src/main/java/com/lizin5ths/indypets/config/client/ItemGuiProvider.java
@@ -7,17 +7,15 @@ import me.shedaniel.autoconfig.util.Utils;
 import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import me.shedaniel.clothconfig2.impl.builders.DropdownMenuBuilder;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.Item;
+
 import java.lang.reflect.Field;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ItemGuiProvider implements GuiProvider {
 	// Cloth Config understands null as an error, thus we use a "null object"
@@ -27,12 +25,16 @@ public class ItemGuiProvider implements GuiProvider {
 		if (id.isEmpty())
 			return NULL; // translate to null later
 
-		Identifier identifier = Identifier.tryParse(id);
+		var identifier = Identifier.tryParse(id);
 
-		if (!BuiltInRegistries.ITEM.containsKey(identifier))
+		if (identifier == null || !BuiltInRegistries.ITEM.containsKey(identifier))
 			return null; // show as error
 
 		return identifier;
+	}
+
+	private static String toText(Identifier id) {
+		return id == NULL ? "" : id.toString();
 	}
 
 	public static Identifier get(Field field, Object config) {
@@ -54,24 +56,39 @@ public class ItemGuiProvider implements GuiProvider {
 	@SuppressWarnings("rawtypes")
 	@Override
 	public List<AbstractConfigListEntry> get(String i13n, Field field, Object config, Object defaults, GuiRegistryAccess registry) {
-		return Collections.singletonList(
-			ConfigEntryBuilder.create()
-				.startDropdownMenu(Component.translatable("text.autoconfig.indypets.option.interactItem"),
-					DropdownMenuBuilder.TopCellElementBuilder.of(
-						ItemGuiProvider.get(field, config),
-						ItemGuiProvider::toObject,
-						value -> Component.literal(value == NULL ? "" : value.toString())),
-					DropdownMenuBuilder.CellCreatorBuilder.ofItemIdentifier())
-				.setDefaultValue(NULL)
-				.setSelections(
-					Stream.concat(
-							BuiltInRegistries.ITEM.stream()
-								.sorted(Comparator.comparing(Item::toString))
-								.map(BuiltInRegistries.ITEM::getKey),
-							Stream.of(NULL))
-						.collect(Collectors.toCollection(LinkedHashSet::new)))
-				.setSaveConsumer(newValue -> set(field, config, newValue))
-				.build()
-		);
+		var description = Component.translatable("text.autoconfig.indypets.option.interactItem");
+		var value = get(field, config);
+		var selections = BuiltInRegistries.ITEM.stream()
+			.sorted(Comparator.comparing(Item::toString))
+			.map(BuiltInRegistries.ITEM::getKey)
+			.collect(Collectors.toCollection(LinkedHashSet::new));
+
+		if (Minecraft.getInstance().level != null) {
+			return Collections.singletonList(
+				ConfigEntryBuilder.create()
+					.startDropdownMenu(description,
+						DropdownMenuBuilder.TopCellElementBuilder.of(
+							value,
+							ItemGuiProvider::toObject,
+							v -> Component.literal(toText(v))),
+						DropdownMenuBuilder.CellCreatorBuilder.ofItemIdentifier())
+					.setDefaultValue(NULL)
+					.setSelections(selections)
+					.setSaveConsumer(newValue -> set(field, config, newValue))
+					.build()
+			);
+		} else {
+			// workaround for broken item dropdown in 26.1 when no world is loaded
+			return Collections.singletonList(
+				ConfigEntryBuilder.create()
+					.startStringDropdownMenu(description,
+						toText(value),
+						Component::literal)
+					.setDefaultValue(toText(NULL))
+					.setSelections(selections.stream().map(ItemGuiProvider::toText).toList())
+					.setSaveConsumer(newValue -> 	set(field, config, toObject(newValue)))
+					.build()
+			);
+		}
 	}
 }
