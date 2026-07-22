fabb8bc8c9a43f15941eb265b9fa1c7063769d74
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-07-22 20:02:56 +0200

    let config codec throw and encode the actual payload

--- src/main/java/com/lizin5ths/indypets/network/PlayerConfigPayload.java
+++ src/main/java/com/lizin5ths/indypets/network/PlayerConfigPayload.java
@@ -1,6 +1,5 @@
 package com.lizin5ths.indypets.network;
 
-import com.google.gson.JsonSyntaxException;
 import com.lizin5ths.indypets.IndyPets;
 import com.lizin5ths.indypets.config.Config;
 import net.minecraft.network.FriendlyByteBuf;
@@ -11,19 +10,14 @@ public record PlayerConfigPayload(Config config) implements CustomPacketPayload 
 	public static final Type<PlayerConfigPayload> TYPE = new CustomPacketPayload.Type<>(IndyPets.id("player_config"));
 
 	public static final StreamCodec<FriendlyByteBuf, PlayerConfigPayload> CODEC = StreamCodec.ofMember(
-		(value, buf) -> {
-			String json = Config.GSON_EXCLUDE_LOCAL.toJson(Config.local());
+		(payload, buf) -> {
+			String json = Config.GSON_EXCLUDE_LOCAL.toJson(payload.config());
 			buf.writeUtf(json);
 		},
 		buf -> {
 			String json = buf.readUtf(32767);
-			try {
-				Config config = Config.GSON_EXCLUDE_LOCAL.fromJson(json, Config.class);
-				return new PlayerConfigPayload(config);
-			} catch (JsonSyntaxException e) {
-				IndyPets.LOGGER.error("couldn't parse received player config! \"{}\"", json, e);
-				return null;
-			}
+			Config config = Config.GSON_EXCLUDE_LOCAL.fromJson(json, Config.class);
+			return new PlayerConfigPayload(config);
 		});
 
 	@Override
